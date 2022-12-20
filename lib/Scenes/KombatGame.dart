d71bf183ff7f2c2e616244f425fbd8b48409a7aa
Yony BM <4yony4@gmail.com>
2022-12-20 12:42:02 +0100

    Agregando un objeto con colisiones

--- lib/Scenes/KombatGame.dart
+++ lib/Scenes/KombatGame.dart
@@ -3,11 +3,14 @@
 import 'dart:ui';
 
 import 'package:flame/game.dart';
+import 'package:flame_forge2d/forge2d_game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
-class KombatGame extends FlameGame{
+class KombatGame extends Forge2DGame{
 
   late TiledComponent mapComponent;
+
+  KombatGame():super(gravity: Vector2(0, 9.8), zoom: 1);
 
   @override
   Future<void>? onLoad() async{
