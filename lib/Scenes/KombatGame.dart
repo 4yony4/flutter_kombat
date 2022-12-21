3fad7a89fb4ce4bedbbdd52b169fcba134e88547
Yony BM <4yony4@gmail.com>
2022-12-21 10:33:31 +0100

    Creando objeto propio colision BodyComponent

--- lib/Scenes/KombatGame.dart
+++ lib/Scenes/KombatGame.dart
@@ -6,6 +6,7 @@ import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flame_forge2d/forge2d_game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flutterkombat/Objects/TiledMapObjectBody.dart';
 import 'package:flutterkombat/Players/Fighter.dart';
 
 class KombatGame extends Forge2DGame{
@@ -26,70 +27,16 @@ class KombatGame extends Forge2DGame{
     final capaObjGroup = mapComponent.tileMap.getLayer<ObjectGroup>('objetos');
 
     for (final obj in capaObjGroup!.objects) {
-      addTiledObjectCollision(obj);
+      TiledMapObjectBody tmob=TiledMapObjectBody(obj);
+      add(tmob);
     }
 
-    Fighter fighter=Fighter(position: Vector2(size.x/3,0), size: size*0.1);
+    Fighter fighter=Fighter(position: Vector2(size.x/3,100), size: size*0.1);
     add(fighter);
 
 
   }
 
-  void addTiledObjectCollision(TiledObject tiledObject){
-
-    //DEFINO PRIMERO EL CUERPO Y NO LA FORMA
-    final bodyDef = BodyDef()
-      ..position = Vector2(tiledObject.x, tiledObject.y)
-      ..type = BodyType.static;
-    Body body=world.createBody(bodyDef);
-
-
-    //DEFINO LA FORMA
-
-    if(tiledObject.isPolygon) {
-      ChainShape shape = ChainShape();
-
-      final List<Vector2> vertices = [];
-      for (final polygon in tiledObject.polygon) {
-        //print("DEBUG: POINTS ----->>>>> "+polygon.x.toString()+"   "+polygon.y.toString());
-        //vertices.add(Vector2(polygon.x, polygon.y));
-        shape.vertices.add(Vector2(polygon.x, polygon.y));
-      }
-      final fixtureDef = FixtureDef(shape)
-        ..userData = this // To be able to determine object in collision
-        ..restitution = 0.8
-        ..density = 1.0
-        ..friction = 0.2;
-
-      body.createFixture(fixtureDef);
-    }
-    else if(tiledObject.isRectangle){
-      print("DEBUG: ---->>>> "+tiledObject.x.toString());
-      final shape = PolygonShape();
-
-      final vertices = [
-        /*Vector2(-size.x / 2, size.y / 2),
-      Vector2(size.x / 2, size.y / 2),
-      Vector2(0, -size.y / 2),*/
-        Vector2(0, 0),
-        Vector2(tiledObject.width, 0),
-        Vector2(tiledObject.width, tiledObject.height),
-        Vector2(0, tiledObject.height),
-      ];
-      shape.set(vertices);
-
-      //final shape = CircleShape()..radius = size.x / 2;
-      final fixtureDef = FixtureDef(shape)
-        ..userData = this // To be able to determine object in collision
-        ..restitution = 0.8
-        ..density = 1.0
-        ..friction = 0.2;
-
-      body.createFixture(fixtureDef);
-    }
-    //else if(tiledObject.isPolygon) {
-  }
-  
   @override
   Color backgroundColor() {
     // TODO: implement backgroundColor
